e03c559980e5cff638d93d43d166bdd961b190a4
Avenshy <30952834+Avenshy@users.noreply.github.com>
2019-11-13 00:03:07 +0800

    update 0.3dev
    0.3dev 更新内容：
    1.跳过答题，直接签到
    2.优化代码，修复bug

--- YibanCheckin.py
+++ YibanCheckin.py
@@ -3,16 +3,17 @@ import json
 import configparser
 import os
 
+
 def YibanCheckin():
     username = input('Username: ')
     password = input('Password: ')
     print('Login......',end='')
-    url = 'https://mobile.yiban.cn/api/v2/passport/login?account=' + username + '&passwd=' + password + '&v=9.9.9&ct=99' # + '&app=0&apn=wifi&identify=123456789&sig=a47a6db4540b4e7e&token=&device=unknown%3Aunknown&sversion=28'
+    url = 'https://mobile.yiban.cn/api/v2/passport/login?v=9.9.9&ct=99&account=' + urllib.request.quote(username) + '&passwd=' + urllib.request.quote(password)  #+'&app=0&apn=wifi&identify=123456789&sig=a47a6db4540b4e7e&token=&device=unknown%3Aunknown&sversion=28'
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         data = response.read()
         json_data = data.decode()
-    if json_data.find('\\u8bf7\\u6c42\\u6210\\u529f') > 0: #请求成功
+    if '\\u8bf7\\u6c42\\u6210\\u529f' in json_data: #请求成功
         print('Success!')
         config = configparser.ConfigParser()
         config.read('option.ini')
@@ -39,47 +40,46 @@ def YibanCheckin():
         print('token: ' + token)
         json_data = json_data['user']
         print("name: " + json_data['name'])
-        url = 'http://mobile.yiban.cn/api/v3/checkin/question?access_token=' + token
-        print('Question...',end='')
-        req = urllib.request.Request(url)
+#        url = 'http://mobile.yiban.cn/api/v3/checkin/question?access_token=' + token
+#        print('Question...',end='')
+#        req = urllib.request.Request(url)
+#        with urllib.request.urlopen(req) as response:
+#            data = response.read()
+#            json_data = data.decode()
+#        if json_data.find('\\u8bf7\\u6c42\\u6210\\u529f') > 0: #请求成功
+#            print('Success!')
+#            json_data = json.loads('%s' % json_data)
+#            json_data = json_data['data']
+#            if json_data['isChecked'] is True:
+#                print('\n\n[You have already cheakin!!!]')
+#                exit(0)
+#            if('survey' not in json_data):
+#                print('[WRONG!!]')
+#                exit(0)
+#            json_data = json_data['survey']
+#            json_data = json_data['question']
+#            print('id: ' + json_data['id'],'title: ' + json_data['title'])
+#            json_data = json_data['option']
+#            id = json_data[0]['id']
+#            print('Checkin......',end='')
+        url = 'http://mobile.yiban.cn/api/v3/checkin/answer?access_token=' + token + '&optionId=666666' #+ id
+        req = urllib.request.Request(url,data={})
         with urllib.request.urlopen(req) as response:
             data = response.read()
             json_data = data.decode()
-        if json_data.find('\\u8bf7\\u6c42\\u6210\\u529f') > 0: #请求成功
-            print('Success!')
+        if '\\u8bf7\\u6c42\\u6210\\u529f' in json_data:
             json_data = json.loads('%s' % json_data)
             json_data = json_data['data']
-            if json_data['isChecked'] is True:
-                print('\n\n[You have already cheakin!!!]')
-                exit(0)
-            json_data = json_data['survey']
-            json_data = json_data['question']
-            print('id: ' + json_data['id'],'title: ' + json_data['title'])
-            json_data = json_data['option']
-            id = json_data[0]['id']
-            print('Checkin......',end='')
-            url = 'http://mobile.yiban.cn/api/v3/checkin/answer?access_token=' + token + '&optionId=' + id
-            req = urllib.request.Request(url,data={})
-            with urllib.request.urlopen(req) as response:
-                data = response.read()
-                json_data = data.decode()
-
-            if json_data.find('\\u8bf7\\u6c42\\u6210\\u529f') > 0:
-                json_data = json.loads('%s' % json_data)
-                json_data = json_data['data']
+            if 'money' in json_data:
                 print('\n\n[Success!!]')
             else:
-                print('Fail!')
-                json_data = json.loads('%s' % json_data)
-                print(json_data)
-        else:
-            print('Fail!')
-            json_data = json.loads('%s' % json_data)
-            print(json_data)
-    else:
-        print('Fail!')
-        json_data = json.loads('%s' % json_data)
-        print(json_data)
+                print('\n\n[You have already cheakin!!!]')
+            os.system('pause')
+            exit(0)
+    print('Fail!')
+    json_data = json.loads('%s' % json_data)
+    print(json_data)
+
 def YibanCheckin_Auto():
     config = configparser.ConfigParser()
     config.read('option.ini')
@@ -90,62 +90,59 @@ def YibanCheckin_Auto():
         i+=1
         print(i,username,end=' ')
         password = config.get(username,'password')
-        url = 'https://mobile.yiban.cn/api/v2/passport/login?account=' + username + '&passwd=' + password + '&v=9.9.9&ct=99' #+
-                                                                                                                             #'&ct=0&app=0&v=4.6.13&apn=wifi&identify=123456789&sig=a47a6db4540b4e7e&token=&device=unknown%3Aunknown&sversion=28'
+        url = 'https://mobile.yiban.cn/api/v2/passport/login?account=' + urllib.request.quote(username) + '&passwd=' + urllib.request.quote(password) + '&v=9.9.9&ct=99' #+'&ct=0&app=0&v=4.6.13&apn=wifi&identify=123456789&sig=a47a6db4540b4e7e&token=&device=unknown%3Aunknown&sversion=28'
         req = urllib.request.Request(url)
         with urllib.request.urlopen(req) as response:
             data = response.read()
             json_data = data.decode()
-        if json_data.find('\\u8bf7\\u6c42\\u6210\\u529f') > 0: #请求成功
+        if '\\u8bf7\\u6c42\\u6210\\u529f' in json_data: #请求成功
             json_data = json.loads('%s' % json_data)
             json_data = json_data['data']
             token = json_data['access_token']
             json_data = json_data['user']
             print(json_data['name'],end=' ')
-            url = 'http://mobile.yiban.cn/api/v3/checkin/question?access_token=' + token
-            req = urllib.request.Request(url)
+#            url = 'http://mobile.yiban.cn/api/v3/checkin/question?access_token=' + token
+#            req = urllib.request.Request(url)
+#            with urllib.request.urlopen(req) as response:
+#                data = response.read()
+#                json_data = data.decode()
+#            if '\\u8bf7\\u6c42\\u6210\\u529f' in json_data: #请求成功
+#                json_data = json.loads('%s' % json_data)
+#                json_data = json_data['data']
+#                if json_data['isChecked'] is True:
+#                    print('[Already cheakin!!]')
+#                    fail+=1
+#                    continue
+#                if('survey' not in json_data):
+#                    print('[WRONG!!]')
+#                    fail+=1
+#                    continue
+#                json_data = json_data['survey']
+#                json_data = json_data['question']
+#                json_data = json_data['option']
+#                id = json_data[0]['id']
+            url = 'http://mobile.yiban.cn/api/v3/checkin/answer?access_token=' + token + '&optionId=666666' #+ id
+            req = urllib.request.Request(url,data={})
             with urllib.request.urlopen(req) as response:
                 data = response.read()
                 json_data = data.decode()
-            if json_data.find('\\u8bf7\\u6c42\\u6210\\u529f') > 0: #请求成功
+            if '\\u8bf7\\u6c42\\u6210\\u529f' in json_data:
                 json_data = json.loads('%s' % json_data)
                 json_data = json_data['data']
-                if json_data['isChecked'] is True:
-                    print('[Already cheakin!!]')
-                    fail+=1
-                    continue
-                json_data = json_data['survey']
-                json_data = json_data['question']
-                json_data = json_data['option']
-                id = json_data[0]['id']
-                url = 'http://mobile.yiban.cn/api/v3/checkin/answer?access_token=' + token + '&optionId=' + id
-                req = urllib.request.Request(url,data={})
-                with urllib.request.urlopen(req) as response:
-                    data = response.read()
-                    json_data = data.decode()
-                if json_data.find('\\u8bf7\\u6c42\\u6210\\u529f') > 0:
-                    json_data = json.loads('%s' % json_data)
-                    json_data = json_data['data']
-                    print('[Success!!!]')
+                if 'money' in json_data:
+                    print('[Success!!]')
                     success+=1
                 else:
-                    print('[Fail!]',end='')
-                    json_data = json.loads('%s' % json_data)
-                    print(json_data)
+                    print('[You have already cheakin!!!]')
                     fail+=1
-            else:
-                print('[Fail!]',end='')
-                json_data = json.loads('%s' % json_data)
-                print(json_data)
-                fail+=1
-        else:
-            print('[Fail!]',end='')
-            json_data = json.loads('%s' % json_data)
-            print(json_data)
-            fail+=1
+                continue
+        print('[Fail!] ',end='')
+        json_data = json.loads('%s' % json_data)
+        print(json_data)
+        fail+=1
     print('\n\nAll done!!All:',i,'Success:',success,'Fail:',fail)
 
-print('**********  Created By Avenshy  **********\nVer 0.2dev')
+print('**********  Created By Avenshy  **********\nVer 0.3dev')
 try:
     f = open('option.ini')
     f.close()
@@ -157,11 +154,9 @@ while back not in ['Y','N']:
     back = input('Auto mode? Press Y or N.').upper()
     if(back == 'Y'):
         YibanCheckin_Auto()
-        print('\n\n\nFinish!!')
         os.system('pause')
         exit(0)
     elif(back == 'N'):
         YibanCheckin()
-        print('\n\n\nFinish!!')
         os.system('pause')
         exit(0)
